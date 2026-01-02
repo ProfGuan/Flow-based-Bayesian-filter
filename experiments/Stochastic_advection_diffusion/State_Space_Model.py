a72b4b78a6843ad3ebd0ca0582a80ab66bdc6b3c
ProfGuan <guanxf@tongji.edu.cn>
2026-01-02 16:17:05 +0800

    Refactor State_Space_Model with updated parameters
    Updated spatial step size calculation and modified source function. Removed unused code and adjusted observation handling.

--- experiments/Stochastic_advection_diffusion/State_Space_Model.py
+++ experiments/Stochastic_advection_diffusion/State_Space_Model.py
@@ -19,7 +19,6 @@ warnings.filterwarnings("ignore")
 class Stochastic_AD_System:
     
     def __init__(self, params_dict, sigma, obs_type, sensor_index):
-        
         L = params_dict["L"]
         T = params_dict["T"]
         self.D = params_dict["D"]            # Diffusion parameter
@@ -27,7 +26,7 @@ class Stochastic_AD_System:
         self.Nx = params_dict["Nx"]
         self.Nt = params_dict["Nt"]
         
-        self.dx = L / (self.Nx - 1)     # Spatial step size
+        self.dx = 2 * L / (self.Nx - 1)     # Spatial step size
         self.Dt = T / self.Nt           # assimilation time step
         self.dt = self.Dt / 10          # Time step size
 
@@ -36,16 +35,12 @@ class Stochastic_AD_System:
         
         # Create spatial and temporal grids
         self.x = np.linspace(-L, L, self.Nx)
-#         self.t = np.linspace(0, T, self.Nt)
 
         self.sigma = sigma
-#         self.r2 = r2
         self.obs_type = obs_type
         self.sensor_index = sensor_index
-#         self.n = obs_dim
 
         # Initial and boundary conditions
-#         self.s_0 = np.zeros(self.Nx)        # Initial condition: s(x, 0) = 0
         self.s_0 = self.initial(self.x)
         
         # Sparse matrix for the implicit scheme
@@ -57,39 +52,29 @@ class Stochastic_AD_System:
         self.A = sp.diags(diagonals, offsets=[-1, 0, 1], format='csc')
         
         self.m = self.Nx
-#         self.n = self.Nx
         
     def initial(self, x):
         return -np.sin(np.pi * x)
     
     def source(self, x):
-#         return 2 * x * (x - 1) - 0.2        
         return 5 * (x + 1) * (x - 1)
 
     def h(self, x, obs_type):
         if obs_type == 'I':
             return self.h_I(x)
         elif obs_type == 'NL':
-            return self.h_nonlinear(x)
-        elif obs_type == 'partial':
-            return self.h_partial(x)
-        elif obs_type == 'PN':
-            return self.h_PN(x)
+            return self.h_NL(x)
     
     def h_I(self, x):
         return x
-    
-    def h_partial(self, x):
-        return x[0:self.Nx//2]
 
-    def h_PN(self, x):
+    def h_NL(self, x):
         return np.exp(-x + 0.5)
         
     def forward_step(self, s_old, F, z):
         rhs = s_old + self.dt * F + z
 
         # Solve the linear system A * s_new = rhs
-#         s_new = np.zeros_like(s_old)
         rhs[0] = 0
         rhs[-1] = 0
         
@@ -99,24 +84,16 @@ class Stochastic_AD_System:
         self.A[-1, -1] = 1
         
         s_new = spla.spsolve(self.A, rhs)
-        
-        # apply homo dirichlet BC
-#         s_new[1:-1] = s_new_in
-#         s_new[0] = 0
-#         s_new[-1] = 0
         return s_new
     
     def solve_AD(self, s_0, dt, Dt, trans_noise):
         s_old = s_0    # Placeholder for the next time step
         F = self.source(self.x)
-#         obs_noise_sample = obs_noise.rvs(size=[1, self.Nt])
 
         n_iter = int(Dt / dt)
         S = np.zeros([self.Nt, self.m])
         S[0] = self.s_0
-#         O = np.zeros([self.Nt, self.n])
-#         O[0] = self.h(s_0, self.obs_type) + obs_noise_sample[0]
-        
+
         for n in range(self.Nt-1):
             trans_noise_sample = trans_noise.rvs(size=[1, n_iter])
             
@@ -129,19 +106,14 @@ class Stochastic_AD_System:
                 s_old = s_new.copy()
                 
             S[n+1] = s_new
-#             O[n+1] = self.h(s_new, self.obs_type) + obs_noise_sample[i]
         return S        
     
     def generate_state_sample(self, N_train, N_test):
         Q = self.sigma ** 2 * self.dt * np.eye(self.Nx)
-#         R = self.r2 * np.eye(self.n)
         trans_noise = multivariate_normal(mean=np.zeros(self.m), cov=Q)
-#         obs_noise = multivariate_normal(mean=np.zeros(self.n), cov=R)
         
         x_train = np.zeros([N_train, self.Nt, self.m])
-#         y_train = np.zeros([N_train, self.Nt, self.n])
         x_test = np.zeros([N_test, self.Nt, self.m])
-#         y_test = np.zeros([N_test, self.Nt, self.n])
         
         print("==> Generate training samples:")
         i = 0
